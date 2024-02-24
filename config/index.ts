fd133cb8e1fb42ade6bc780b0937bc041b63cb13
jack-kwan <34941961+jack-kwan@users.noreply.github.com>
2024-02-24 10:49:45 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: '聊天APP',
+  description: '智能聊天机器人（助理/客服/企业知识库）',
+  copyright: '唐道科技 http://www.tangdao.vip',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
